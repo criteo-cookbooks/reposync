160273e22edbadf5d47aefe53d4ea2d046399bdc
Jeremy MAURO <j.mauro@criteo.com>
2017-12-04 20:40:55 +0100

    Bump to 0.1.1 + Remove not needed dependency
    Change-Id: Idf6a735e2dd40a738bddccc7a799d03537dd9f21

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ description      'Installs/Configures reposync'
 long_description 'reposync Cookbook'
 issues_url       'https://github.com/criteo-cookbooks/reposync/issue' if respond_to?(:issues_url)
 source_url       'https://github.com/criteo-cookbooks/reposync' if respond_to?(:source_url)
-version          '0.1.0'
+version          '0.1.1'
 supports         'centos'
-depends          'yum-centos'
 chef_version     '>= 12.5' if respond_to?(:chef_version)
